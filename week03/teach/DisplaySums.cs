3c7652731a0f47856dae611ccfb0795d86ccfcd4
ItsMen <mvdo1091@gmail.com>
2024-07-01 21:59:36 -0400

    Completed weekly team activity

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -29,5 +29,15 @@
     /// <param name="numbers">array of integers</param>
     private static void DisplaySumPairs(int[] numbers) {
         // TODO Problem 2 - This should print pairs of numbers in the given array
+        var values = new HashSet<int>();
+
+        foreach (var number in numbers)
+        {
+            var summand = 10 - number;
+            if (values.Contains(summand))
+                Console.WriteLine($"{number} {summand}");
+                
+            values.Add(number);
+        }
     }
 }
